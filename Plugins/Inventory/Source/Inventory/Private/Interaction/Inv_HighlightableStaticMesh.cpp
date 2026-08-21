26a63f9e9b6d7de621fb83d3f1e5936b7a783bc5
Cris Nanu <cristian.nanu@hotmail.com>
2026-08-20 22:07:54 -0400

    Inventory Highlightable Items added

---
+++ Plugins/Inventory/Source/Inventory/Private/Interaction/Inv_HighlightableStaticMesh.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright Nourda
+
+
+#include "Interaction/Inv_HighlightableStaticMesh.h"
+
+void UInv_HighlightableStaticMesh::Highlight_Implementation()
+{
+	SetOverlayMaterial(HighlightMaterial);
+	
+}
+
+void UInv_HighlightableStaticMesh::UnHighlight_Implementation()
+{
+	SetOverlayMaterial(nullptr);
+}
--- Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -4,6 +4,7 @@
 #include "Player/Inv_PlayerController.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "Interaction/Inv_Highlightable.h"
 #include "Items/Components/Inv_ItemComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "Widgets/HUD/Inv_HUDWidget.h"
@@ -91,6 +92,11 @@ void AInv_PlayerController::TraceForItem()
 	
 	if (ThisActor.IsValid())
 	{
+		if (UActorComponent* Highlightable = ThisActor->FindComponentByInterface(UInv_Highlightable::StaticClass()); IsValid(Highlightable))
+		{
+			IInv_Highlightable::Execute_Highlight(Highlightable);
+		}
+		
 		UInv_ItemComponent* ItemComponent = ThisActor->FindComponentByClass<UInv_ItemComponent>();
 		if (!IsValid(ItemComponent)) return;
 		
@@ -99,7 +105,10 @@ void AInv_PlayerController::TraceForItem()
 	
 	if (LastActor.IsValid())
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Stopped Tracing last Actor"));
+		if (UActorComponent* Highlightable = LastActor->FindComponentByInterface(UInv_Highlightable::StaticClass()); IsValid(Highlightable))
+		{
+			IInv_Highlightable::Execute_UnHighlight(Highlightable);
+		}
 	}
 	
 }
